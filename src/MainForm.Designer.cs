98fd16e8a112f9b9c1481343ac05c94d2921369d
Juan Lopes <me@juanlopes.net>
2011-06-06 19:29:15 -0300

    some layout adjustments

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -49,9 +49,9 @@
             this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.StartButton.Font = new System.Drawing.Font("Webdings", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.StartButton.ForeColor = System.Drawing.Color.White;
-            this.StartButton.Location = new System.Drawing.Point(152, 34);
+            this.StartButton.Location = new System.Drawing.Point(138, 34);
             this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(48, 47);
+            this.StartButton.Size = new System.Drawing.Size(45, 47);
             this.StartButton.TabIndex = 1;
             this.StartButton.Text = "4";
             this.StartButton.UseVisualStyleBackColor = false;
@@ -65,9 +65,9 @@
             this.ResetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ResetButton.Font = new System.Drawing.Font("Webdings", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.ResetButton.ForeColor = System.Drawing.Color.White;
-            this.ResetButton.Location = new System.Drawing.Point(206, 34);
+            this.ResetButton.Location = new System.Drawing.Point(183, 34);
             this.ResetButton.Name = "ResetButton";
-            this.ResetButton.Size = new System.Drawing.Size(47, 47);
+            this.ResetButton.Size = new System.Drawing.Size(45, 47);
             this.ResetButton.TabIndex = 2;
             this.ResetButton.Text = "9";
             this.ResetButton.UseVisualStyleBackColor = false;
@@ -86,9 +86,9 @@
             this.TimeLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.TimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TimeLabel.ForeColor = System.Drawing.Color.Transparent;
-            this.TimeLabel.Location = new System.Drawing.Point(12, 33);
+            this.TimeLabel.Location = new System.Drawing.Point(12, 34);
             this.TimeLabel.Name = "TimeLabel";
-            this.TimeLabel.Size = new System.Drawing.Size(132, 47);
+            this.TimeLabel.Size = new System.Drawing.Size(120, 47);
             this.TimeLabel.TabIndex = 3;
             this.TimeLabel.Text = "12:34";
             this.TimeLabel.UseVisualStyleBackColor = false;
@@ -103,7 +103,7 @@
             this.CommitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CommitButton.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.CommitButton.ForeColor = System.Drawing.Color.White;
-            this.CommitButton.Location = new System.Drawing.Point(179, -2);
+            this.CommitButton.Location = new System.Drawing.Point(153, -2);
             this.CommitButton.Name = "CommitButton";
             this.CommitButton.Size = new System.Drawing.Size(30, 30);
             this.CommitButton.TabIndex = 14;
@@ -124,7 +124,7 @@
             this.TopMostCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.TopMostCheck.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.TopMostCheck.ForeColor = System.Drawing.Color.White;
-            this.TopMostCheck.Location = new System.Drawing.Point(209, -2);
+            this.TopMostCheck.Location = new System.Drawing.Point(183, -2);
             this.TopMostCheck.Name = "TopMostCheck";
             this.TopMostCheck.Size = new System.Drawing.Size(30, 30);
             this.TopMostCheck.TabIndex = 13;
@@ -142,7 +142,7 @@
             this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CloseButton.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.CloseButton.ForeColor = System.Drawing.Color.White;
-            this.CloseButton.Location = new System.Drawing.Point(239, -2);
+            this.CloseButton.Location = new System.Drawing.Point(213, -2);
             this.CloseButton.Name = "CloseButton";
             this.CloseButton.Size = new System.Drawing.Size(30, 30);
             this.CloseButton.TabIndex = 12;
@@ -159,7 +159,7 @@
             this.SettingsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SettingsButton.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.SettingsButton.ForeColor = System.Drawing.Color.White;
-            this.SettingsButton.Location = new System.Drawing.Point(152, -2);
+            this.SettingsButton.Location = new System.Drawing.Point(126, -2);
             this.SettingsButton.Name = "SettingsButton";
             this.SettingsButton.Size = new System.Drawing.Size(30, 30);
             this.SettingsButton.TabIndex = 15;
@@ -173,15 +173,15 @@
             this.GotoLink.BackColor = System.Drawing.Color.DarkGreen;
             this.GotoLink.DisabledLinkColor = System.Drawing.Color.White;
             this.GotoLink.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.GotoLink.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GotoLink.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.GotoLink.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
             this.GotoLink.LinkColor = System.Drawing.Color.White;
-            this.GotoLink.Location = new System.Drawing.Point(0, 88);
+            this.GotoLink.Location = new System.Drawing.Point(0, 89);
             this.GotoLink.Name = "GotoLink";
-            this.GotoLink.Size = new System.Drawing.Size(269, 23);
+            this.GotoLink.Size = new System.Drawing.Size(243, 23);
             this.GotoLink.TabIndex = 17;
             this.GotoLink.TabStop = true;
-            this.GotoLink.Text = "http://github.com/juanplopes/dojotimer";
+            this.GotoLink.Text = "github.com/juanplopes/dojotimer";
             this.GotoLink.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.GotoLink.VisitedLinkColor = System.Drawing.Color.White;
             this.GotoLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GotoLink_LinkClicked);
@@ -193,7 +193,7 @@
             this.TitleBar.ForeColor = System.Drawing.Color.White;
             this.TitleBar.Location = new System.Drawing.Point(0, 0);
             this.TitleBar.Name = "TitleBar";
-            this.TitleBar.Size = new System.Drawing.Size(269, 28);
+            this.TitleBar.Size = new System.Drawing.Size(243, 28);
             this.TitleBar.TabIndex = 4;
             // 
             // MainForm
@@ -201,8 +201,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
-            this.ClientSize = new System.Drawing.Size(269, 111);
+            this.ClientSize = new System.Drawing.Size(243, 112);
             this.ControlBox = false;
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.StartButton);
             this.Controls.Add(this.GotoLink);
             this.Controls.Add(this.SettingsButton);
             this.Controls.Add(this.CommitButton);
@@ -210,8 +212,6 @@
             this.Controls.Add(this.CloseButton);
             this.Controls.Add(this.TitleBar);
             this.Controls.Add(this.TimeLabel);
-            this.Controls.Add(this.ResetButton);
-            this.Controls.Add(this.StartButton);
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
